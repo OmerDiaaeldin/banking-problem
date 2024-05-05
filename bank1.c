abbd4450ccf513816377972ef5aabb3403b6697c
Omer Diaaeldin <omardiaaeldin2592001@gmail.com>
2024-05-05 03:37:06 +0100

    first commit

---
+++ bank1.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<semaphore.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<unistd.h>
+#include<stdlib.h>
+#define SHM_KEY 12345
+#define SHM_SIZE 1024
+
+void update(sem_t *s1, char *shm, int amount,int sleep_duration){
+    sem_wait(s1);
+    printf("Bank 1 change: %d\n", amount);
+    int balance = atoi(shm);
+    sprintf(shm,"%d",balance+amount);
+    printf("New balance: %d\n",balance+amount);
+    sleep(sleep_duration);
+    sem_post(s1);
+}
+
+int main(){
+    sem_t *s1;
+    s1 = sem_open("/first", O_EXCL);
+
+    int shmid;
+    int key = SHM_KEY;
+    char *shm;
+    /* create shared memory segment */
+    shmid = shmget(key, SHM_SIZE, 0666);
+    if (shmid < 0) {
+        perror("shmget");
+        exit(1);
+    }
+    /* attach shared memory segment */
+    shm = shmat(shmid, NULL, 0);
+    if (shm == (char *) -1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    int balance = atoi(shm);
+
+    update(s1,shm,20,4);
+    sleep(2);
+
+    update(s1,shm,50,2);
+    sleep(3);
+
+    update(s1,shm,70,3);
+    sleep(1);
+}
---
+++ bank2.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<semaphore.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<unistd.h>
+#include<stdlib.h>
+#define SHM_KEY 12345
+#define SHM_SIZE 1024
+
+void update(sem_t *s1, char *shm, int amount,int sleep_duration){
+    sem_wait(s1);
+    printf("Bank 2 change: %d\n", amount);
+    int balance = atoi(shm);
+    sprintf(shm,"%d",balance+amount);
+    printf("New balance: %d\n",balance+amount);
+    sleep(sleep_duration);
+}
+
+int main(){
+    sem_t *s1;
+    s1 = sem_open("/first", O_EXCL);
+
+    int shmid;
+    int key = SHM_KEY;
+    char *shm;
+    /* create shared memory segment */
+    shmid = shmget(key, SHM_SIZE, 0666);
+    if (shmid < 0) {
+        perror("shmget");
+        exit(1);
+    }
+    /* attach shared memory segment */
+    shm = shmat(shmid, NULL, 0);
+    if (shm == (char *) -1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    int balance = atoi(shm);
+
+    update(s1,shm,10,4);
+    sleep(2);
+
+    update(s1,shm,40,2);
+    sleep(3);
+
+    update(s1,shm,60,3);
+    sleep(1);
+}
